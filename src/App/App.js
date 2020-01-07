c5bd4010157d60b88d9cdf21003fd04fe983adff
alesha reed <aleshak83@gmail.com>
2020-01-06 21:36:49 -0600

    added employees

--- src/App/App.js
+++ src/App/App.js
@@ -1,25 +1,30 @@
 import React from 'react';
 import DogPen from '../components/DogPen/DogPen';
 import dogData from '../helpers/data/dogsData';
+import employeeData from '../helpers/data/employeesData';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss';
+import StaffRoom from '../components/StaffRoom/StaffRoom';
 
 
 class App extends React.Component {
   state = {
     dogs: [],
+    employees: [],
   }
 
   componentDidMount() {
     const dogs = dogData.getAllDogs();
-    this.setState({ dogs });
+    const employees = employeeData.getAllEmployees();
+    this.setState({ dogs, employees });
   }
 
   render() {
     return (
       <div className="App">
         <DogPen dogs={this.state.dogs} />
+        <StaffRoom employees={this.state.employees} />
       </div>
     );
   }
---
+++ src/helpers/data/employeesData.js
@@ -0,0 +1,30 @@
+const employees = [
+  {
+    id: 'employee1',
+    firstName: 'Gabriel',
+    lastName: 'Bowers',
+    phoneNumber: 9017361211,
+  },
+  {
+    id: 'employee2',
+    firstName: 'Yonette',
+    lastName: 'West',
+    phoneNumber: 7313130099,
+  },
+  {
+    id: 'employee3',
+    firstName: 'Edward',
+    lastName: 'Minor',
+    phoneNumber: 7313130089,
+  },
+  {
+    id: 'employee4',
+    firstName: 'Adrienne',
+    lastName: 'Lyles',
+    phoneNumber: 6154330099,
+  },
+];
+
+const getAllEmployees = () => employees;
+
+export default { getAllEmployees };

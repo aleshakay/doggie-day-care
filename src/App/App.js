b35664b5e6ebe6a04e82a45a0aa29097134ca13e
alesha reed <aleshak83@gmail.com>
2020-01-06 21:06:00 -0600

    added dog and setup

--- src/App/App.js
+++ src/App/App.js
@@ -1,29 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import DogPen from '../components/DogPen/DogPen';
+import dogData from '../helpers/data/dogsData';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.scss';
 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <button className='btn btn-primary'>Shark</button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    dogs: [],
+  }
+
+  componentDidMount() {
+    const dogs = dogData.getAllDogs();
+    this.setState({ dogs });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <DogPen dogs={this.state.dogs} />
+      </div>
+    );
+  }
 }
-
 export default App;
